9a60a397d5bcc67c03ab96ee23478a98644e7365
edmond120 <edmondwong888@msn.com>
2017-10-02 11:31:56 -0400

    added my_strcpy

--- striny.c
+++ striny.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-
+char* my_strncpy(char* destination, char* target){
+	char * index;
+	for(index = destination; *target != '\0'; index++){
+		*index = *target;
+		target++;
+	}
+	*index = *target;
+	return destination;
+}
 
 int main(){
-
 }
