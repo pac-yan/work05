2f234eda4f0f89c61582fb4fd35aa3fa7fa3cac9
Pacy Yan <pyan@stuy.edu>
2017-10-04 11:29:29 -0400

    wrote strcat

--- striny.c
+++ striny.c
@@ -19,9 +19,35 @@ int mystrlen(char *str){
     }
     return(len);
 }
+char mystrcat(char *dest, char *add) {
+  int x = 0;
+  char retstr[strlen(dest) + strlen(add)]; 
+  for(x = 0; x < strlen(dest); x ++) {
+    retstr[x] = dest[x];      
+  }
+  int i = x;
+  int z = 0;
+  for(i; i < strlen(retstr); i ++) {
+    for(z; z < strlen(add); z ++){
+      restr[i] = add[z];
+      retstr[i] = *(add + cont); 
+    }
+  }
+  return retstr;  
+}
+
+
 int main(){
-  char str1[] = "magic";
-  printf("str1 is: \"%s\"\n", str1);//should print magic
-  printf("true len of str1 is: %d\n", strlen(str1));
-  printf("mylength of str1 is: %d\n", mystrlen(str1));//should return five
+  printf("strlen tests: \n");
+  char strlen1[] = "magic";
+  printf("str1 is: \"%s\"\n", strlen1);//should print magic
+  printf("true len of str1 is: %d\n", strlen(strlen1));
+  printf("mylength of str1 is: %d\n", mystrlen(strlen1));//should return five
+  printf("strcat test: \n");
+  char strcat1[] = "hi";
+  char strcat2[] = " there";
+  printf("str1 is: \"%s\"\n", strcat1);//should print "hi"
+  printf("str1 is: \"%s\"\n", strcat2);//should print "there"
+  printf("true cat of str1 is: \"%s\"\n", strcat(strcat1, strcat2));
+  printf("mycat is: \"%s\"\n", mystrcat(strcat1, strcat2));//should return "hi there"
 }
